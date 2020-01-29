c4685c7ed5a95538019c9489db357bb0b09a9f14
luca <luca.carnaroli98@gmail.com>
2020-01-29 19:25:04 +0100

    es 3

--- js/script.js
+++ js/script.js
@@ -13,48 +13,46 @@
 // }
 // Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
 // creo un array
-var studentiOgg = [
-  // creo oggetti
-  {
-    'nome':'Luca',
-    'cognome':'Carnaroli',
-    'eta': 21,
-  },
-  {
-    'nome':'Maria',
-    'cognome':'Rossi',
-    'eta': 32,
-  },
-  {
-    'nome':'Gigi',
-    'cognome':'Bianchi',
-    'eta': 17,
-  },
-];
-console.log(studentiOgg);
-// stampo nome e cognome
-for (var i = 0; i < studentiOgg.length; i++) {
-  console.log(studentiOgg[i].nome);
-  console.log(studentiOgg[i].cognome);
-  console.log(studentiOgg[i].eta);
-}
-
+// var studentiOgg = [
+//   // creo oggetti
+//   {
+//     'nome':'Luca',
+//     'cognome':'Carnaroli',
+//     'eta': 21,
+//   },
+//   {
+//     'nome':'Maria',
+//     'cognome':'Rossi',
+//     'eta': 32,
+//   },
+//   {
+//     'nome':'Gigi',
+//     'cognome':'Bianchi',
+//     'eta': 17,
+//   },
+// ];
+// console.log(studentiOgg);
+// // stampo nome e cognome
+// for (var i = 0; i < studentiOgg.length; i++) {
+//   console.log(studentiOgg[i].nome +' '+ studentiOgg[i].cognome +' '+ studentiOgg[i].eta);
+// }
+//
 
 // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
 // fare 3 prompt
-// var nome = prompt('come ti chiami?');
-// var cognome = prompt('il tuo cognome?');
-// var eta = parseInt(prompt('quanti anni hai?'));
-// var studenteObj = [
-//   {
-//     'nome'
-//     'cognome'
-//     'eta'
-//   }
-// ];
-// console.log(studenteObj[]);
-//
-//
-//
-// console.log(studenteObj);
+var studenteObj = [
+  {
+    'nome': 'Matteo',
+    'cognome': 'Rossi',
+    'eta': 20
+  },
+  {
+    'nome': prompt('inserisci il tuo nome'),
+    'cognome': prompt('inserisci il tuo cognome'),
+    'eta': parseInt(prompt('inserisci la tua eta'))
+  }
+];
+for (var dati in studenteObj) {
+  console.log(dati, studenteObj[dati]);
+};
