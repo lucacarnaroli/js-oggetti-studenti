452db206680913e0bc9aba09f15e854da538f1d9
luca <luca.carnaroli98@gmail.com>
2020-01-29 16:10:09 +0100

    esercizio

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+
+// creo un oggetto
+var studente = {
+  // descrivo uno studente
+  'nome':'Luca',
+  'cognome':'Carnaroli',
+  'eta': 21
+}
+console.log(studente);
